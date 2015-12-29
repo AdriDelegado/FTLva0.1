85c509187fdfc45375fd46b31d864c4b2987c2e3
NunoRCurado <nuno.romao.curado@gmail.com>
2015-12-29 03:40:13 +0000

    sala

--- FTL/FTL/sala.h
+++ FTL/FTL/sala.h
@@ -5,11 +5,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "consola.h"
 
 using namespace std;
 
 class sala
 {
+	int x;
+	int y;
 	int id;
 	int oxigenio;
 	int integridade;
@@ -17,12 +20,14 @@ class sala
 	bool fogo;
 	bool curtoCircuito;  // o que é cc ?
 	int numeroOcupantes;
+	
 
 	
 public:
 	sala(int id);
 	sala();
 	~sala(void);
+
 
 	//------GET---------
 	virtual int getId() = 0;
@@ -32,6 +37,8 @@ public:
 	virtual bool getFogo() = 0;
 	virtual bool getCurtoCircuito() = 0;  // o que é cc ?
 	virtual int getNumeroOcupantes() = 0;
+	virtual int getX() = 0;
+	virtual int getY() = 0;
 	
 	//------SET---------
 	virtual void setId(int id) = 0;
@@ -41,6 +48,8 @@ public:
 	virtual void setFogo(bool newValorFogo) = 0;
 	virtual void setCurtoCircuito(bool newValorCC) = 0;  // o que é cc ?
 	virtual void setNumeroOcupantes(int newValorOcupantes)=0;
+	virtual void setX(int x) = 0;
+	virtual void setY(int y) = 0;
 
 };
 
